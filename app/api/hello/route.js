05d071653c1761d8e89d1acc15b22b9f1ae7e533
jatin bansal <jbansal1305@gmail.com>
2023-07-02 20:07:06 +0530

    first commit

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,9 +1,1 @@
-export async function GET(request) {
-  return Response.json([
-    { distance: "45-minute drive" },
-    { distance: "4.5-hour drive" },
-    { distance: "4.5-hour drive" },
-    { distance: "6-hour drive" },
-    { distance: "2-hour drive" },
-  ]);
-}
+
